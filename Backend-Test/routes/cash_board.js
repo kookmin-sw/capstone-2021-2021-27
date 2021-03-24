eeaf53196aba9998f407508a6088ef22cbd1b406
jung211p <jung211p@kookmin.ac.kr>
2021-03-25 02:12:23 +0900

    db update&back-end update

--- Backend-Test/routes/cash_board.js
+++ Backend-Test/routes/cash_board.js
@@ -15,37 +15,55 @@ connection.connect();
 router.get('/', function(req, res, next) {
     if(!req.session.idx)
         res.render('login', { title: 'have to login' });
-    else
-        res.render('cash_board', { title: req.session.nickname });
 
-});
-
-
-router.post('/', function(req, res, next) {
-
-    connection.query(util.format('CALL `capstone_27`.`reqDeposit`(\'%s\', \'%s\');', req.session.idx, req.body.dps), function(err, results, fields) {
+    connection.query(util.format('CALL `capstone_27`.`getKrwInfo`(\'%s\');', req.session.idx), function(err, results, fields) {
         if (err) {
             console.log('deposit request failed');
         }
         else {
             console.log('sucess deposit request');
-            res.render('cash_board')
+            var total = results[1][0]['TotalKRW'];
+            var withdraw = results[1][0]['Withdraw_KRW'];
+            var worked = results[1][0]['Worked_KRW'];
+            var available = results[1][0]['Available_KRW'];
+
+            res.render('cash_board', { title: 'total price', total: total, withdraw: withdraw, worked: worked, available: available });
+
         }
     });
+
+
 });
 
 
 router.post('/', function(req, res, next) {
 
-    connection.query(util.format('CALL `capstone_27`.`reqWithdraw`(\'%s\', \'%s\');', req.session.idx, req.body.wd), function(err, results, fields) {
-        if (err) {
-            console.log('withdraw request failed');
-        }
-        else {
-            console.log(results);
+    if(!req.body.wd) {
+        connection.query(util.format('CALL `capstone_27`.`reqDeposit`(\'%s\', \'%s\');', req.session.idx, req.body.dps), function(err, results, fields) {
+            if (err) {
+                console.log('deposit request failed');
+            }
+            else {
+                console.log('sucess deposit request');
+                res.send(results[0][0]["result"]);
+            }
+        });
 
-        }
-    });
+    }
+    else {
+        connection.query(util.format('CALL `capstone_27`.`reqWithdraw`(\'%s\', \'%s\');', req.session.idx, req.body.wd), function(err, results, fields) {
+            if (err) {
+                console.log('withdraw request failed');
+            }
+            else {
+                console.log(results);
+                res.send(results[1][0]["result"]);
+
+            }
+        });
+    }
+
 });
+
 
 module.exports = router;
--- Backend-Test/routes/manager.js
+++ Backend-Test/routes/manager.js
@@ -2,6 +2,8 @@ var express = require('express');
 var mysql = require('mysql');
 var router = express.Router();
 var util = require('util');
+var bodyParser = require('body-parser');
+
 var connection = mysql.createConnection({
     host     : 'capstone.louissoft.kr',
     port     : '26773',
@@ -11,49 +13,64 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    res.render('manager');
+});
+
+// Get list
+router.post('/', function(req, res, next) {
+
     connection.query('CALL `capstone_27`.`cashReqList`();', function(err, results, fields) {
         if (err) {
             console.log('deposit request failed');
         }
         else {
             var resultmsg = JSON.stringify(results[0]);
-            console.log(resultmsg);
             res.send(resultmsg);
-            // res.render('manager')
-        }
-    });
-
-
-});
-
-
-router.post('/', function(req, res, next) {
-
-    connection.query('CALL `capstone_27`.`cashReqList`();', function(err, results, fields) {
-        if (err) {
-            console.log('deposit request failed');
-        }
-        else {
-            console.log(results);
-            res.render('cash_board')
         }
     });
 });
 
+// confirm
+router.post('/confirm', function(req, res) {
+    // idx, type - Withdraw, Deposit
+    // if
+    console.log('confirm');
+    var type = req.body.type;
+    var check_idx = req.body.idx;
+    console.log(type);
+    console.log(check_idx);
+    req.session.check_idx = check_idx;
+    if(type == 0){
+        connection.query(util.format('CALL `capstone_27`.`resDeposit`(\'%s\');',req.session.check_idx), function(err, results, fields) {
+            if (err) {
+                console.log('deposit response failed');
+            }
+            else {
+                console.log(results);
+                res.send(results[0][0]['result']);
+            }
+        });
+    }
+    else if (type == 1){
+        connection.query(util.format('CALL `capstone_27`.`resWithdraw`(\'%s\');',req.session.check_idx), function(err, results, fields) {
+            if (err) {
+                console.log('withdraw response failed');
+            }
+            else {
+                console.log(results);
+                res.send(results[0][0]['result']);
+            }
+        });
+    }
+    else {
+        console.log('exception');
+    }
 
-router.post('/', function(req, res, next) {
-
-    connection.query(util.format('CALL `capstone_27`.`reqWithdraw`(\'%s\', \'%s\');', req.session.idx, req.body.wd), function(err, results, fields) {
-        if (err) {
-            console.log('withdraw request failed');
-        }
-        else {
-            console.log(results);
-
-        }
-    });
 });
 
 module.exports = router;
