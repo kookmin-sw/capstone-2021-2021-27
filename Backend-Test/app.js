d99391230e55c5d9b0781490f32d5f8b757d7f8e
jung211p <jung211p@kookmin.ac.kr>
2021-03-27 19:46:09 +0900

    file_upload back-end

--- Backend-Test/app.js
+++ Backend-Test/app.js
@@ -40,6 +40,7 @@ app.use('/join', require('./routes/join'));
 app.use('/dupCheck', require('./routes/dupCheck'));
 app.use('/cash_board', require('./routes/cash_board'));
 app.use('/manager', require('./routes/manager'));
+app.use('/fileUpload', require('./routes/fileUpload'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
