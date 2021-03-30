abd664a66e2b7a4b13957d96bf8a84e6d068a609
jung211p <jung211p@kookmin.ac.kr>
2021-03-30 19:06:44 +0900

    cash_board upgrade

--- Backend-Test/routes/cash_board.js
+++ Backend-Test/routes/cash_board.js
@@ -14,10 +14,11 @@ connection.connect();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     if(!req.session.idx)
-        res.render('login', { title: 'have to login' });
+        res.render('login');
 
     connection.query(util.format('CALL `capstone_27`.`getKrwInfo`(\'%s\');', req.session.idx), function(err, results, fields) {
         if (err) {
+
             console.log('deposit request failed');
         }
         else {
@@ -26,6 +27,12 @@ router.get('/', function(req, res, next) {
             var withdraw = results[1][0]['Withdraw_KRW'];
             var worked = results[1][0]['Worked_KRW'];
             var available = results[1][0]['Available_KRW'];
+
+            if(withdraw == '')
+                withdraw = '0';
+            if(worked =='')
+                worked='0';
+
 
             res.render('cash_board', { title: 'total price', total: total, withdraw: withdraw, worked: worked, available: available });
 
@@ -38,8 +45,8 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
 
-    if(!req.body.wd) {
-        connection.query(util.format('CALL `capstone_27`.`reqDeposit`(\'%s\', \'%s\');', req.session.idx, req.body.dps), function(err, results, fields) {
+    if(req.body.mode == 'deposit') {
+        connection.query(util.format('CALL `capstone_27`.`reqDeposit`(\'%s\', \'%s\');', req.session.idx, req.body.param), function(err, results, fields) {
             if (err) {
                 console.log('deposit request failed');
             }
@@ -51,7 +58,7 @@ router.post('/', function(req, res, next) {
 
     }
     else {
-        connection.query(util.format('CALL `capstone_27`.`reqWithdraw`(\'%s\', \'%s\');', req.session.idx, req.body.wd), function(err, results, fields) {
+        connection.query(util.format('CALL `capstone_27`.`reqWithdraw`(\'%s\', \'%s\');', req.session.idx, req.body.param), function(err, results, fields) {
             if (err) {
                 console.log('withdraw request failed');
             }
