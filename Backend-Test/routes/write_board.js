435a85996c73406f6b6cf58075125145337d886d
jihopark <wlgh2213@naver.com>
2021-03-31 18:05:12 +0900

    Write board finish

--- Backend-Test/routes/write_board.js
+++ Backend-Test/routes/write_board.js
@@ -14,8 +14,28 @@ let storage = multer.diskStorage({
         let extension = path.extname(file.originalname);
         let basename = path.basename(file.originalname, extension);
         var fname = uuid.v4() + extension;
+        var index = req.files.length - 1;
+        var labelData = JSON.parse(req.body.labelData);
+        var nowData = labelData[index];
 
-        callback(null, fname);
+        var x = nowData['x'];
+        var y = nowData['y'];
+        var width = nowData['width'];
+        var height = nowData['height'];
+
+        if(!req.body.labelIdx)
+            req.body.labelIdx = '';
+
+        connection.query(util.format('CALL `capstone_27`.`fileUpload`(\'img\',\'%s\',  \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\');',fname, width, height, x, y, 1, req.session.idx +';'), function(err, results, fields) {
+            if (err) {
+                console.log('error');
+                callback(null, fname);
+            }
+            else {
+                req.body.labelIdx += (results[0][0]["file_idx"]) + ';';
+                callback(null, fname);
+            }
+        });
 
 
     }
@@ -45,21 +65,23 @@ router.get('/', function(req, res, next) {
 
 router.post('/', upload.array('imgFile', 5), function(req, res, next) {
     let files = req.files
-    console.log(req.files);
-    console.log(req.body);
 
-    let result = {
-        originalName : files[0].originalname,
-        size : files[0].size,
-        text : req.body.postText,
-    }
+    console.log(util.format('CALL `capstone_27`.`writeBoard`(\'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\');',req.body.title, req.session.idx, req.body.price, req.body.content, req.body.quantity, 'img', req.body.labelIdx));
 
-    res.json(result);bnvcxzcdvbfjmyihtdfgcsxzc vbnm,kjhgfdvxcdsfszxasqdwgzszb ad vefvf
+    connection.query(util.format('CALL `capstone_27`.`writeBoard`(\'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\');',req.body.title, req.session.idx, req.body.price, req.body.content, req.body.quantity, 'img', req.body.labelIdx), function(err, results, fields) {
+        if (err) {
+            console.log('error');
+        }
+        else {
+            res.send('finish');
+        }
+    });
+
+
 
 
 
 
 });
-
 
 module.exports = router;
