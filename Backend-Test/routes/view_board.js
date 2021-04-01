c5f348fc43651595730c0ff56ea62e5833323442
jung211p <jung211p@kookmin.ac.kr>
2021-04-02 05:26:30 +0900

    view board upgrade

--- Backend-Test/routes/view_board.js
+++ Backend-Test/routes/view_board.js
@@ -37,26 +37,31 @@ router.get('/', function(req, res, next) {
             res.render('view_board',{ title : title, content : content, price:price, writer_idx:writer_idx, quantity: quantity, file_idx:split_example_file_idx, total_price:total_price, status:status, date:date});
         }
     });
+});
 
+router.get('/api/:bidx', function(req, res, next) {
+    console.log('api');
+    connection.query('CALL `getQABoard`(\''+req.params.bidx+'\')', function(err, results, fields) {
+        if (err) {
 
+        }
+        else {
+            res.json(results[0]);
+        }
+    });
 });
 
 router.post('/', function(req, res, next) {
-
-    if(req.body.mode == 'question') {
-        connection.query(util.format('CALL `capstone_27`.`writeQuestion`(\'%s\',\'%s\',\'%s\');',  req.query.board_idx, req.body.param, req.session.idx), function(err, results, fields) {
-            if (err) {
-                console.log('write question request failed');
-            }
-            else {
-                console.log('sucess write question request');
-                console.log(results);
-                res.send('ㅇㅇ');
-                // res.send(results[0][0]["result"]);
-            }
-        });
-
-    }
+    connection.query(util.format('CALL `capstone_27`.`writeQuestion`(\'%s\',\'%s\',\'%s\');',  req.query.board_idx, req.body.qa_text, req.session.idx), function(err, results, fields) {
+        if (err) {
+            console.log('write question request failed');
+            res.send('error');
+        }
+        else {
+            console.log('sucess write question request');
+            res.redirect('/view_board/?board_idx=' + req.query.board_idx);
+        }
+    });
 
 });
 
