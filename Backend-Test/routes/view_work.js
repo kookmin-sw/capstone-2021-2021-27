ae65c5e5a8f23f2751064f3a9e0421ced97bbc67
jung211p <jung211p@kookmin.ac.kr>
2021-05-22 03:20:57 +0900

    web backend update

--- Backend-Test/routes/view_work.js
+++ Backend-Test/routes/view_work.js
@@ -15,6 +15,8 @@ connection.connect();
 router.get('/', function(req, res, next) {
     if(!req.session.idx)
         res.render('login');
+
+    res.render('view_work');
 });
 
 router.get('/api', function(req, res, next) {
@@ -31,6 +33,22 @@ router.get('/api', function(req, res, next) {
             res.send(results[0]);
 
 
+        }
+    });
+});
+
+router.get('/admit/:fidx', function(req, res, next) {
+    if(!req.session.idx)
+        res.render('login');
+
+    connection.query(util.format('CALL `capstone_27`.`admitWork`(\'%s\');', req.params.fidx), function(err, results, fields) {
+        if (err) {
+            console.log('admit work error');
+            res.send('error');
+        }
+        else {
+            console.log('sucess admit work');
+            res.redirect('/view_work');
         }
     });
 });
