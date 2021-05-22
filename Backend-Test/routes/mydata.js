e201683f8cef4674fcd7622dd45b61073d96792b
jung211p <jung211p@kookmin.ac.kr>
2021-05-22 16:27:51 +0900

    Web css edit my page add

---
+++ Backend-Test/routes/mydata.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var mysql = require('mysql');
+var router = express.Router();
+var util = require('util');
+const multer = require("multer");
+const path = require("path");
+var uuid = require("uuid");
+
+var connection = mysql.createConnection({
+    host     : 'capstone.louissoft.kr',
+    port     : '26773',
+    user     : 'capstone_27',
+    password : 'zoqtmxhs27wh!',
+    database : 'capstone_27'
+});
+connection.connect();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    if(!req.session.idx)
+        res.render('login');
+    else
+        res.render('mydata');
+
+});
+
+module.exports = router;
